ed49e5bf277816871fccd3fa2cf706202b68e587
Gabriel Desjardins <gdesjardin@teksystems.com>
2018-05-29 14:38:20 -0400

    go go

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <html>
 <body>
-helloo
+<h2>Blog</h2>
+                <div id="feed" class="rssFeed"><div class="rssBody"><ul>
+                <?php
+                  require('../magpierss-master/rss_fetch.inc');
+                  $url = 'http://environnement.ca/blog/?feed=rss2';
+                  $rss = fetch_rss($url);
+
+                  foreach ( $rss->items as $item ) {
+                          // echo "Title: " . utf8_encode($item['title']);
+                          //echo "Published: " . date("n/j/o h:i A", $published);
+                          // echo "Content: " . utf8_encode(implode(",", $item['content']));
+                          echo '<li class="rssRow odd">';
+                          echo '<h4>';
+                          echo '<a href="';
+                          echo  $item['link'];
+                          echo  '" title="" target="_self">';
+                          echo utf8_encode($item['title']);
+                          echo '</a></h4><div>';
+                          //echo date("n/j/o h:i A", $item['pubdate');
+                          echo '</div>';
+                          echo utf8_encode(implode(",", $item['content']));
+                          echo '</li>';
+
+                  }
+                ?>
+                </ul></div></div>
 </body>
 </html>

23ad8d67dbf3a47c59f49d25e65b3a9a55b8a481
Gabriel Desjardins <gdesjardin@teksystems.com>
2018-05-29 14:44:39 -0400

    Go

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <body>
 <h2>Blog</h2>
                 <div id="feed" class="rssFeed"><div class="rssBody"><ul>
+                --	<?php echo 1+2 ?> --
                 <?php
                   require('../magpierss-master/rss_fetch.inc');
                   $url = 'http://environnement.ca/blog/?feed=rss2';

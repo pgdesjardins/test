5731b69d84903dc65da35d0dace7d202dca0b92a
Gabriel Desjardins <gdesjardin@teksystems.com>
2018-06-05 10:30:47 -0400

    go

--- index.php
+++ index.php
@@ -34,5 +34,22 @@ include '../magpierss-master/rss_fetch.inc';
                   // }
                 ?>
                 </ul></div></div>
+                
+                <?php
+                $url = "http://www.realbeta.net63.net/blog/rss?id=1";
+$xml = simplexml_load_file($url);
+for($i = 0; $i < 1; $i++){
+  
+  $title = $xml->channel->item[$i]->title;
+  $link = $xml->channel->item[$i]->link;
+  $description = $xml->channel->item[$i]->description;
+  $pubDate = $xml->channel->item[$i]->pubDate;
+  
+
+  echo "<a target='_blank' href='$link'><b>$title</b></a>"; // Title of post
+  echo "$description"; // Description
+  echo "<br />$pubDate<br /><br />"; // Date Published
+  ?>
+}
 </body>
 </html>
